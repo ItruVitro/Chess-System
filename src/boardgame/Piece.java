0ebaa57458c91f37c35c19ccbb2eb907dbca2163
Italo Silva <italo.vct@gmail.com>
2024-06-20 18:10:43 -0300

    Chess layer and printing the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -5,8 +5,9 @@ public class Piece {
 	protected Position position;
 	private Board board;
 	
-	public Piece(Position position, Board board) {
+	public Piece(Board board) {
 		this.board = board;
+		position = null;
 	}
 
 	protected Board getBoard() {
